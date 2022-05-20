badb7b821eecb1956179de434547e7b1ca218162
Oleksandr Parfionov <parfyonator@gmail.com>
2022-05-20 16:31:57 +0300

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:health_kit_reporter/health_kit_reporter.dart';
+import 'package:health_kit_reporter/model/payload/heartbeat_series.dart';
+import 'package:health_kit_reporter/model/payload/preferred_unit.dart';
+import 'package:health_kit_reporter/model/payload/quantity.dart';
+import 'package:health_kit_reporter/model/predicate.dart';
+import 'package:health_kit_reporter/model/type/quantity_type.dart';
+
+import './widgets.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Stream<int>hbpm = const Stream<int>.empty();
+  
+  @override
+  void initState() {
+    super.initState();
+    hbpm = Stream<int>.periodic(const Duration(seconds: 1), (x) => x);
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Health Example'),
+          ),
+          body: FutureBuilder(
+            future:  HealthKitReporter.requestAuthorization([QuantityType.heartRate.identifier], []),
+            builder: (context, snapshot) {
+              if( snapshot.hasData && snapshot.data == true )
+                return StreamBuilder(
+                  stream: hbpm,
+                  builder: (context, snapshot) => HBPM(),
+                );
+              
+              return const Center(child: CircularProgressIndicator(strokeWidth: 6,));
+            }
+          ),
+      )
+    );
+  }
+}
---
+++ lib/widgets.dart
@@ -0,0 +1,76 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:health_kit_reporter/health_kit_reporter.dart';
+import 'package:health_kit_reporter/model/payload/heartbeat_series.dart';
+import 'package:health_kit_reporter/model/payload/preferred_unit.dart';
+import 'package:health_kit_reporter/model/payload/quantity.dart';
+import 'package:health_kit_reporter/model/predicate.dart';
+import 'package:health_kit_reporter/model/type/quantity_type.dart';
+
+Future<List<Quantity>> getData() async {
+  final now = DateTime.now();
+  final prefUnits = await HealthKitReporter.preferredUnits([QuantityType.heartRate]);
+  final hrUnits = prefUnits.first.unit;
+  final hbQuery = await HealthKitReporter.quantityQuery(
+    QuantityType.heartRate,
+    hrUnits,
+    Predicate(now.subtract(Duration(seconds: 120)), now)
+  );
+  hbQuery.sort(((Quantity a, Quantity b) => b.endTimestamp.compareTo(a.endTimestamp)));
+
+  print(hbQuery.map((e) => e.harmonized.value));
+
+  return hbQuery;
+}
+
+class HBPM extends StatelessWidget {  
+  HBPM({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: getData(),
+      // initialData: InitialData,
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        if( !snapshot.hasData ){
+          print('Snapshot is empty.');
+          return const Center(
+            child: Text(
+              'Snapshot is empty.',
+              style: TextStyle(
+                fontSize: 20,
+              ),
+            ),
+          );
+        }
+        
+        final healthData = snapshot.data as List<Quantity>;
+        if( healthData.isNotEmpty ){
+          final hb = healthData.first;
+          print('#####################');
+          print('HBPM: ${hb.harmonized.value}');
+          print('#####################');
+          return Center(
+            child: Text(
+              'HBPM: ${hb.harmonized.value}',
+              style: const TextStyle(
+                fontSize: 20,
+              ),
+            ),
+          );
+        }
+
+        print('No heartbeat data.');
+        return const Center(
+          child: Text(
+            'No heartbeat data available',
+            style: TextStyle(
+              fontSize: 20,
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
